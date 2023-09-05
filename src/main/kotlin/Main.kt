5d4cb2b85adf3d8e2edaaa5df7a92fdbada79b21
Dschids <123191756+Dschids@users.noreply.github.com>
2023-09-04 22:29:57 -0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    // create an array of at least 5 items, use a for loop to print them
+    var ammo = arrayOf("Pistol", "Rifle", "Shotgun", "Sniper Rifle", "Energy Cells")
+    println("Ammo Types:")
+    for (item in ammo) {
+        println(item)
+    }
+}
